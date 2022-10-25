27a512b63abf81846f1514658b932dea92835d43
Hirunika Gunathunga <84180743+HirunikaGunathunga@users.noreply.github.com>
2022-10-25 20:09:38 +0530

    customize

--- src/components/workerstable/clientTable.jsx
+++ src/components/workerstable/clientTable.jsx
@@ -1,55 +1,194 @@
 import "./workerstable.scss"
-//import { DataGrid } from '@mui/x-data-grid';
-//import { userRows, userColumns } from "../../workersTableSource";
 import { Link, useNavigate } from 'react-router-dom';
-
 import {useEffect, useState} from 'react'
 import axios from "axios";
+import * as React from 'react';
 import './style.css'
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Select from 'react-select';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import Box from '@mui/material/Box';
 
 
-  
-const Workerstable = () => {
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios.get('http://localhost:1337/api/worker/customer').then((response)=>{setData(response.data)})
-  }, [])
-  //console.log(data.fName)
-//   const navigate = useNavigate();
-//   const viewPro = () => { axios.put('http://localhost:1337/api/worker')}
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
 
-  
-  
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
   return (
-
-    <div className="workerstable">
-      <table className="table">
-          <thead className="th">
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>District</th>
-              <th>Profile</th>
-              
-          </thead>
-          <tbody className="tb">
-            {data.map((x) => <tr style={{ 'marginLeft': "5rem" }} className="tr" key={x._id}>
-              <td className="td">{x.fName}</td>
-              <td className="td">{x.lName}</td>
-              <td className="td">{x.district}</td>
-              
-              <Link to= { `/workers/customer/${x._id}`}className="linkStlyes"> 
-                   <td className="td-A"><Button variant="outlined" color="secondary" size="small">View</Button></td>
-              </Link>
-              
-            </tr>)}
-          </tbody>
-  </table>
-        
-    </div>
-  )
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
 }
 
-export default Workerstable
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+
+export default function CustomPaginationActionsTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = useState([]);
+  const [query, setQuery] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:1337/api/worker/customer').then((response)=>{setRows(response.data)})
+  }, [])
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  return (
+    <TableContainer component={Paper} sx={{ maxWidth: 1200 }}>
+      <Table sx={{ minWidth: 650 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="left">First Name</StyledTableCell>
+            <StyledTableCell align="left">Last Name</StyledTableCell>
+            <StyledTableCell align="left">Email</StyledTableCell>
+            <StyledTableCell align="left">Distict</StyledTableCell>
+            <StyledTableCell align="left">Profile</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row" style={{ width: 300 }}>
+                {row.fName}
+              </TableCell>
+              <TableCell style={{ width: 300 }} align="left">
+              {row.lName}
+              </TableCell>
+              <TableCell style={{ width: 300 }} align="left">
+              {row.email}
+              </TableCell>
+              <TableCell style={{ width: 300 }} align="left">
+              {row.district}
+              </TableCell>
+              <TableCell style={{ width: 200 }} align="left">
+                <Link to={ `/workers/customer/${row._id}`} className="linkStlyes" style={{"text-decoration": "none"}}>
+                    <Button variant="outlined" color="success">View</Button>
+                </Link>    
+              </TableCell>
+              
+            </TableRow>
+          ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
+
+
+
+
